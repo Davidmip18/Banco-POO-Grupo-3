e172da5b25544b41130c77540ba5e1d721ca6e99
Davidmip18 <dmip.1.3.2.0@gmail.com>
2026-01-19 11:19:00 -0600

    arreglo de problema

--- Cliente.java
+++ Cliente.java
@@ -1,9 +1,9 @@
-public class CLiente () {
+public class Cliente () {
     private String nombre;
     private String direccion;
     private String telefono;
 
-    public CLiente(String nombre, String direccion, String telefono) {
+    public Cliente(String nombre, String direccion, String telefono) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
