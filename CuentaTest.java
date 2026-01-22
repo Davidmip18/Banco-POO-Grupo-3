a5f252161d4d4bfd9c21df621f9331949972072c
Camila <cdgolondrina@gmail.com>
2026-01-22 12:22:16 -0600

    listooo

--- CuentaTest.java
+++ CuentaTest.java
@@ -1,5 +1,9 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 public class CuentaTest{
     @Test
